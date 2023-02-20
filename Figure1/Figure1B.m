bb7ab360b666489ec816d1a743988eb329390a1a
Kaiyun Guan <kaiyun@live.unc.edu>
2023-02-20 09:34:59 -0500

    Update Figure1B.m

--- Figure1/Figure1B.m
+++ Figure1/Figure1B.m
@@ -1,21 +1,25 @@
-% Here we generated the bifurcation diagram with 1800 - 3500 Cdc42 
+% Generate the bifurcation diagram with 1800 - 3500 Cdc42 
 % and 0 - 1000 Bem1-GEF.
+
+% Number of Cdc42 and Bem1-GEF
 n_Cdc42_list = [1800,2000,2200,2500,2800,3000,3200,3500]; 
 BemGEF_list = 0:0.5:1000; 
-% Number of realizations in each regime.
+% Number of realizations in each parameter regime.
 seeds = 10; 
-% Ripley's K values are calculated from the coordinates of Cdc42-GTP 
-% collected every 10 seconds from 2000 seconds to 4000 seconds of each 
-% simulation, so that produced 201 K values.
+% Ripley's K function values are calculated from the coordinates of active 
+% Cdc42 collected every 10 seconds from 2000 seconds to 4000 seconds of each 
+% realization, so that produced 201 K values. We have 10 realizations for
+% each of two initial conditions. The total number of stored K values for a 
+% parameter regime is:
 standard_num_K = 201*2*seeds; 
-% The file path of stored K values.
+% File path of the stored K values.
 K_file_path = '../K_values/2DCorePolarity';
 
 figure; hold on
 for i = 1:numel(n_Cdc42_list)
     n_Cdc42 = n_Cdc42_list(i);
     % Identify the indices of the three polarity regimes. 
-    % See identify_regimes.m.
+    % See "identify_regimes.m".
     [unpolarized_regime, transient_polarity_regime, polarized_regime, ...
         ~, ~, ~] = identify_regimes(n_Cdc42,BemGEF_list,seeds,...
         standard_num_K,K_file_path);

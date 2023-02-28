e17bc4e95b59b0047c0ec15e91f49853d42ec149
Kathy <kaiyun@live.unc.edu>
2023-02-28 16:13:39 -0500

    Rearrange

--- Figure6/Figure6B.m
+++ Figure6/Figure6B.m
@@ -9,34 +9,36 @@ seeds = 10;
 cmap = cool(seeds);
 % Time in the unit of minutes.
 time = (0:10:8000)/60;
-figure('units','pixels','position',[300 300 1200 300]); 
+figure('units','pixels','position',[300 300 1000 800]); 
 for j = 1:seeds
-    subplot(1,4,1); hold on
+    subplot(2,2,[3,4]); hold on
     % Plot time series of K
     plot(time,K(j,:),'color',cmap(j, :),'linewidth',1)
 end
+% Mark the time point where uniform pheromone was switched to a pheromone
+% gradient.
+patch([0,0,1000/60,1000/60],[0,4,4,0],'r',...
+    'edgecolor','r','facealpha',0.15,'edgealpha',0.15)
+plot([1000/60,1000/60],[0,4],'k-','linewidth',4)
+patch([8000/60,8000/60,1000/60,1000/60],[0,4,4,0],'c',...
+    'edgecolor','c','facealpha',0.15,'edgealpha',0.15)
 ylim([0,4])
-xlim([0,8000/60])
+xlim([0,6000/60])
 xlabel('Time (min)')
 ylabel('K')
-set(gca,'fontsize',25)
+set(gca,'fontsize',35)
 
 % Mark one line of K in black and  plotted its snapshots of Cdc42-GTP
 % at different time points. 
-plot(time,K(10,:),'color','k','linewidth',4)
+plot(time,K(10,:),'color','k','linewidth',6)
 load('Coordinates/Figure6B_coordinates.mat')
 L = 8.8623; % domain length
-subplot(1,4,2)
+subplot(2,2,1)
 plot(x_1,y_1,'b.')
 xticks([]); yticks([]); ylim([0,L]); xlim([0,L]); axis square;
-xlabel('29 min','fontsize',25)
+xlabel('1 min','fontsize',35)
 
-subplot(1,4,3)
+subplot(2,2,2)
 plot(x_2,y_2,'b.')
 xticks([]); yticks([]); ylim([0,L]); xlim([0,L]); axis square;
-xlabel('45 min','fontsize',25)
-
-subplot(1,4,4)
-plot(x_3,y_3,'b.')
-xticks([]); yticks([]); ylim([0,L]); xlim([0,L]); axis square;
-xlabel('64 min','fontsize',25)
+xlabel('45 min','fontsize',35)

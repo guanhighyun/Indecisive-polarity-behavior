b3ec231584f71484a2d9c3a4db23a339739c8dec
Kathy <kaiyun@live.unc.edu>
2023-03-20 09:15:49 -0400

    Update Figure 6E

--- Figure6/Figure6E.m
+++ Figure6/Figure6E.m
@@ -4,7 +4,7 @@
 load('FigureData/Figure6E.mat')
 % Data cleaning for generating the box chart and the swarm chart.
 index = repelem(pheromone_gradient,numel(random_seeds),1);
-data = reshape(time_till_stabilization,1,numel(time_till_stabilization));
+data = reshape(time_till_stabilization2,1,numel(time_till_stabilization));
 % Generate the figure.
 figure('position',[300 300 700 500])
 swarmchart(index,data/60,100,'b','filled'); hold on;
